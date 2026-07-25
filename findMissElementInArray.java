4342e16d2139ec9b5242817eb06c1ffec85a878e
Pranshu Verma <er.pranshuverma68@gmail.com>
2026-07-25 11:15:50 +0530

    Implement method to find missing number in array

---
+++ findMissElementInArray.java
@@ -0,0 +1,21 @@
+class Solution {
+    int missingNum(int arr[]) {
+
+        int n = arr.length + 1;
+
+        int xor1 = 0;
+        int xor2 = 0;
+
+        // XOR of 1 to n
+        for (int i = 1; i <= n; i++) {
+            xor1 ^= i;
+        }
+
+        // XOR of array
+        for (int num : arr) {
+            xor2 ^= num;
+        }
+
+        return xor1 ^ xor2;
+    }
+}
